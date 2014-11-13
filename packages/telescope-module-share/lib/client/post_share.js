37b42810638eb42e037a4f3ce4952719ed35c889
Michael Castleman <michael@smartformguide.com>
2014-11-13 16:35:27 +1100

    fix share

--- packages/telescope-module-share/lib/client/post_share.js
+++ packages/telescope-module-share/lib/client/post_share.js
@@ -1,7 +1,7 @@
 Meteor.startup(function () {
   Template[getTemplate('postShare')].helpers({
     sourceLink: function(){
-      return !!getSiteUrl() + "posts/"+this._id : getSiteUrl() + "posts/"+this._id;
+      return getSiteUrl() + "posts/"+this._id;
      // return !!this.url ? this.url : getSiteUrl() + "posts/"+this._id;
     },
     viaTwitter: function () {
