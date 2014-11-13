65b35135006c400d0079dba4cd74e0dfd81f8c10
Michael Castleman <michael@smartformguide.com>
2014-11-13 18:03:44 +1100

    About Page

--- client/views/posts/modules/post_domain.js
+++ client/views/posts/modules/post_domain.js
@@ -1,8 +1,8 @@
 Template[getTemplate('postDomain')].helpers({
   domain: function(){
     var a = document.createElement('a');
-    a.href = this.asxcode;
 //    a.href = this.url;
-    return a.hostname;
+    //return a.hostname;
+    return this.asxcode;
   }
 });
